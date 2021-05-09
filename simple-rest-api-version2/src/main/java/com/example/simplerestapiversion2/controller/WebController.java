90a025cc4182ff1e9c5c6dea6d8bee90a8f429a1
Kiana <kianabrunberg@csus.edu>
2021-05-09 10:10:03 -0700

    Version2 - can now search for any movie by title, year, category and winner then it returns one movie

--- simple-rest-api-version2/src/main/java/com/example/simplerestapiversion2/controller/WebController.java
+++ simple-rest-api-version2/src/main/java/com/example/simplerestapiversion2/controller/WebController.java
@@ -9,33 +9,50 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
-//import com.example.simplerestapis.models.SampleResponse;
 
 @RestController
 public class WebController {
 
-    //if no movie specified, picks a random one
-    //Query search: http://localhost:8080/Oscars?movie/
+    //Query search: http://localhost:8080/Oscars?search= "movie title, year, etc"
     @RequestMapping("/Oscars")
-    public SampleResponse Oscars(@RequestParam(value = "movie/", defaultValue = "empty query") String movie)
+    public SampleResponse Oscars(@RequestParam(value = "search", defaultValue = "empty query") String search)
             throws IOException {
         SampleResponse response = new SampleResponse();
         ObjectMapper objectMapper = new ObjectMapper();
         File JsonData = new File ("src/main/resources/datahubio_oscar_data_json.json");
         JsonNode root = objectMapper.readTree(JsonData);
 
-        Random randGenerator = new Random();
-        int rand = randGenerator.nextInt(11058);
-        response.setId(rand);
-
-        //this will output a message and then the 4 separate categories from the Json file
-        response.setRandom("**Random movie!!**");
-        response.setCategory(root.at("/" +rand+ "/category").asText());
-        response.setEntity(root.at("/" +rand+ "/entity").asText());
-        response.setWinner(root.at("/" +rand+ "/winner").asText());
-        response.setYear(root.at("/" +rand+ "/year").asText());
+        //search by entity, year, category and winner
+        for ( int i = 0; i < 11058; i++) {
+            if (search.equals(root.at("/" +i+ "/entity").asText())) {
+                response.setId(i);
+                response.setCategory(root.at("/" +i+ "/category").asText());
+                response.setEntity(root.at("/" +i+ "/entity").asText());
+                response.setWinner(root.at("/" +i+ "/winner").asText());
+                response.setYear(root.at("/" +i+ "/year").asText());
+            }
+            else if (search.equals(root.at("/" +i+ "/year").asText())) {
+                response.setId(i);
+                response.setCategory(root.at("/" +i+ "/category").asText());
+                response.setEntity(root.at("/" +i+ "/entity").asText());
+                response.setWinner(root.at("/" +i+ "/winner").asText());
+                response.setYear(root.at("/" +i+ "/year").asText());
+            }
+            else if (search.equals(root.at("/" +i+ "/winner").asText())) {
+                response.setId(i);
+                response.setCategory(root.at("/" +i+ "/category").asText());
+                response.setEntity(root.at("/" +i+ "/entity").asText());
+                response.setWinner(root.at("/" +i+ "/winner").asText());
+                response.setYear(root.at("/" +i+ "/year").asText());
+            }
+            else if (search.equals(root.at("/" +i+ "/category").asText())) {
+                response.setId(i);
+                response.setCategory(root.at("/" +i+ "/category").asText());
+                response.setEntity(root.at("/" +i+ "/entity").asText());
+                response.setWinner(root.at("/" +i+ "/winner").asText());
+                response.setYear(root.at("/" +i+ "/year").asText());
+            }
+        }
         return response;
-
     }
 }
